1defe0f1c28ff6f3b2aaf69995328b119467cfab
anastasis-elite <lyndsbelle@gmail.com>
2026-04-22 14:29:29 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
 
         <p
           style={{
-            marginTop: '32px',
+            marginTop: '64px',
             letterSpacing: '6px',
             fontSize: '14px',
             color: '#c58b57',
