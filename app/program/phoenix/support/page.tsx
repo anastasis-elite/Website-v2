4b98a1faf91c71316389b50a4be9b1259475e3cf
anastasis-elite <lyndsbelle@gmail.com>
2026-04-27 12:33:09 -0500

    Create page.tsx

---
+++ app/program/phoenix/support/page.tsx
@@ -0,0 +1,32 @@
+export default function PhoenixSupportPage() {
+  return (
+    <main style={pageStyle}>
+      <div style={containerStyle}>
+        <h1 style={heroTitleStyle}>
+          There are other ways to step into this.
+        </h1>
+
+        <p style={heroTextStyle}>
+          You don’t have to explain everything to move forward.
+        </p>
+
+        <section style={sectionStyle}>
+          <p style={bodyStyle}>
+            If cost, timing, or external factors are making this feel out of reach,
+            there are alternative entry paths available.
+          </p>
+
+          <p style={bodyStyle}>
+            This is not a barrier. It’s just a different starting point.
+          </p>
+        </section>
+
+        <div style={buttonRowStyle}>
+          <a href="/program/phoenix/support/apply" style={primaryButtonStyle}>
+            Continue
+          </a>
+        </div>
+      </div>
+    </main>
+  )
+}
