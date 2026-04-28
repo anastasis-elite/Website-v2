ffbf93dc18497f9e56d9314c99b135f6233a2461
anastasis-elite <lyndsbelle@gmail.com>
2026-04-28 12:33:58 -0500

    Create StartContent.tsx

---
+++ app/dashboard/assessment/StartContent.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import { useSearchParams } from 'next/navigation'
+import * as styles from '../../../styles/globalstyles'
+
+export default function StartContent() {
+  const searchParams = useSearchParams()
+  const program = searchParams.get('program') || ''
+
+  const programLabel =
+    program === 'ember'
+      ? 'Ember'
+      : program === 'ignite'
+        ? 'Ignite'
+        : program === 'phoenix'
+          ? 'Phoenix'
+          : 'your program'
+
+  return (
+    <main style={styles.pageStyle}>
+      <div style={styles.containerStyle}>
+        {/* your existing content */}
+      </div>
+    </main>
+  )
+}
