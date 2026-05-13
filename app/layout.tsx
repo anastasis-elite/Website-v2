73fbdcd42480125da324a95934ee79f9b477809b
anastasis-elite <lyndsbelle@gmail.com>
2026-05-13 11:36:05 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,6 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <header className="nav">
-          <Link href="/">Anastasis Elite</Link>
           <AuthButton />
         </header>
 
