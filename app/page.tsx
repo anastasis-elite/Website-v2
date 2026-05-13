1dcd0a638122925f5d9b99064e2d29335b3930b4
anastasis-elite <lyndsbelle@gmail.com>
2026-05-13 06:20:19 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -109,7 +109,6 @@ export default function Home() {
               justifyContent: 'center',
               width: '270px',
               height: '64px',
-              padding: '0 24px',
               textAlign: 'center',
               border: '1px solid #c58b57',
               color: '#f5f0e8',
