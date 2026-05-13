e8e836a3c9d89dbc7caf4670e9a624187d2848d2
anastasis-elite <lyndsbelle@gmail.com>
2026-05-13 08:13:56 -0500

    Update page.tsx

--- app/dashboard/workout/page.tsx
+++ app/dashboard/workout/page.tsx
@@ -3,7 +3,7 @@
 import { Suspense, useEffect } from 'react'
 import { useSearchParams } from 'next/navigation'
 
-function WorkoutRedirectContent() {
+async function WorkoutRedirectContent() {
   const searchParams = useSearchParams()
   const client = await getClientData()
   
