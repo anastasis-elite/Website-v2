47eefe6af1ae943e20c43c557a18c2f31a8b756f
anastasis-elite <lyndsbelle@gmail.com>
2026-05-13 10:20:44 -0500

    Update page.tsx

--- app/dashboard/assessment/page.tsx
+++ app/dashboard/assessment/page.tsx
@@ -1,12 +1,5 @@
-'use client'
-
-import { Suspense } from 'react'
 import StartContent from './AssessmentContent'
 
 export default function Page() {
-  return (
-    <Suspense fallback={null}>
-      <StartContent />
-    </Suspense>
-  )
+  return <StartContent />
 }
