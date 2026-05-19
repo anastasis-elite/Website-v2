0b1dbc8c53b0509593a31eb39e39e120a3d9dc24
anastasis-elite <lyndsbelle@gmail.com>
2026-05-19 09:41:50 -0500

    Create Button.tsx

---
+++ components/Button.tsx
@@ -0,0 +1,25 @@
+import Link from 'next/link'
+import * as styles from '@/styles/globalstyles'
+
+export default function Button({
+  href,
+  children,
+  variant = 'primary',
+}: {
+  href: string
+  children: React.ReactNode
+  variant?: 'primary' | 'secondary'
+}) {
+  return (
+    <Link
+      href={href}
+      style={
+        variant === 'primary'
+          ? styles.primaryButtonStyle
+          : styles.secondaryButtonStyle
+      }
+    >
+      {children}
+    </Link>
+  )
+}
