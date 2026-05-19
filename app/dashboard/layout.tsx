fc3ec16a7831bb4787b3a1e8e5c3da8c0b538d22
anastasis-elite <lyndsbelle@gmail.com>
2026-05-19 10:52:14 -0500

    Update layout.tsx

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,4 +1,3 @@
-import Link from 'next/link'
 import { getDashboardContext } from '@/lib/dashboard/getDashboardContext'
 
 export default async function DashboardLayout({
@@ -8,37 +7,5 @@ export default async function DashboardLayout({
 }) {
   await getDashboardContext()
 
-  return (
-    <>
-      <div className="dashboard-menu">
-        <details>
-          <summary>◌</summary>
-
-          <div className="dashboard-dropdown">
-  <Link href="/dashboard">Dashboard</Link>
-
-  <Link href="/dashboard/program">
-    Program
-  </Link>
-
-  <Link href="/dashboard/nutrition">
-    Nutrition
-  </Link>
-
-  <Link href="/dashboard/assessment/start">
-    Assessments
-  </Link>
-
-  <form action="/auth/signout" method="post">
-    <button type="submit" className="dashboard-logout">
-      Logout
-    </button>
-  </form>
-</div>
-        </details>
-      </div>
-
-      {children}
-    </>
-  )
+  return <>{children}</>
 }
