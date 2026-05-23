0a91d087592c08a0dfe38d624a7124ff27b23998
anastasis-elite <lyndsbelle@gmail.com>
2026-05-23 12:31:48 -0500

    Update MeasurementAssessment.tsx

--- components/MeasurementAssessment.tsx
+++ components/MeasurementAssessment.tsx
@@ -583,13 +583,13 @@ function MeasurementDiagram({
     }}
   >
           {/* your corrected lines stay here */}
-          <GuideLine y={175} color={lineColor('neck')} label="Neck" />
-          <GuideLine y={220} color={lineColor('shoulders')} label="Shoulders" />
-          <GuideLine y={250} color={lineColor('bust_chest')} label="Chest" />
-          <GuideLine y={275} color={lineColor('underbust')} label="Underbust" />
-          <GuideLine y={300} color={lineColor('waist')} label="Waist" />
-          <GuideLine y={350} color={lineColor('high_hip')} label="High Hip" />
-          <GuideLine y={360} color={lineColor('hips_glutes')} label="Hips" />
+          <GuideLine y={165} color={lineColor('neck')} label="Neck" />
+          <GuideLine y={185} color={lineColor('shoulders')} label="Shoulders" />
+          <GuideLine y={200} color={lineColor('bust_chest')} label="Chest" />
+          <GuideLine y={215} color={lineColor('underbust')} label="Underbust" />
+          <GuideLine y={250} color={lineColor('waist')} label="Waist" />
+          <GuideLine y={300} color={lineColor('high_hip')} label="High Hip" />
+          <GuideLine y={315} color={lineColor('hips_glutes')} label="Hips" />
 
           <GuideLine
             y={550}
