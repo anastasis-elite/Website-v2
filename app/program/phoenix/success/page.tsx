b27814511cf53d5b03f30e21dc5718d534f7ecb2
anastasis-elite <lyndsbelle@gmail.com>
2026-06-23 15:26:05 -0500

    Update page.tsx

--- app/program/phoenix/success/page.tsx
+++ app/program/phoenix/success/page.tsx
@@ -3,6 +3,7 @@
 import { Suspense } from 'react'
 import { useSearchParams } from 'next/navigation'
 import * as styles from '../../../styles/globalstyles'
+import TrackEvent from '@/components/TrackEvent'
 
 function PhoenixSuccessContent() {
   const searchParams = useSearchParams()
@@ -13,6 +14,7 @@ function PhoenixSuccessContent() {
   const email = searchParams.get('email') || ''
 
   return (
+    <><TrackEvent event="phoenix_success_page_viewed" properties={{ page: 'phoenix_success' }} />
     <main style={styles.pageStyle}>
       <div style={styles.containerStyle}>
         <p style={styles.eyebrowStyle}>Phoenix • Confirmed</p>
@@ -83,6 +85,7 @@ function PhoenixSuccessContent() {
         </div>
       </div>
     </main>
+    </>
   )
 }
 
