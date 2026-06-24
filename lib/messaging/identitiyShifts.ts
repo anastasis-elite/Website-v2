796019b854c873fe1e3d4a1e501b77bb527c34b2
anastasis-elite <lyndsbelle@gmail.com>
2026-06-24 17:20:41 -0500

    Create identitiyShifts.ts

---
+++ lib/messaging/identitiyShifts.ts
@@ -0,0 +1,35 @@
+import { IdentityShift } from './types'
+
+export const identityShiftLibrary: IdentityShift[] = [
+  {
+    id: 'identity_001',
+    text:
+      'The woman you are becoming does not require perfect conditions to care for herself.',
+    tags: ['self-trust'],
+    priority: 4,
+  },
+
+  {
+    id: 'identity_002',
+    text:
+      'You are proving reliability to yourself through repeated action.',
+    tags: ['self-trust', 'consistency'],
+    priority: 4,
+  },
+
+  {
+    id: 'identity_003',
+    text:
+      'Capacity expands when you keep showing up for yourself in small ways.',
+    tags: ['capacity'],
+    priority: 4,
+  },
+
+  {
+    id: 'identity_004',
+    text:
+      'You are becoming someone who can trust herself even when life feels messy.',
+    tags: ['self-trust'],
+    priority: 5,
+  },
+]
