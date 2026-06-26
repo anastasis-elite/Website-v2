1e60bf954d08953597435d9e8ffd7c3f7b93840f
anastasis-elite <lyndsbelle@gmail.com>
2026-06-26 11:18:35 -0500

    Create curiosity.ts

---
+++ marketing/hooks/curiosity.ts
@@ -0,0 +1,55 @@
+export const curiosity = {
+
+purpose:
+"Curiosity opens the door to belief change.",
+
+patterns: [
+
+{
+name:
+"Challenge Belief",
+
+template:
+"What if ______ isn't actually ______?"
+
+},
+
+{
+name:
+"Name the Invisible",
+
+template:
+"You've probably been ______ without realizing ______."
+
+},
+
+{
+name:
+"Mental Model",
+
+template:
+"We've been thinking about ______ backwards."
+
+},
+
+{
+name:
+"Open Loop",
+
+template:
+"Everything changed when I realized ______."
+
+},
+
+{
+name:
+"Reverse Conventional Wisdom",
+
+template:
+"The opposite is probably true."
+
+}
+
+]
+
+}
