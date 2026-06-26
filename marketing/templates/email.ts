521c8f305c320c03ae8cd891379285f00e090af6
anastasis-elite <lyndsbelle@gmail.com>
2026-06-26 12:25:09 -0500

    Create email.ts

---
+++ marketing/templates/email.ts
@@ -0,0 +1,13 @@
+// marketing/templates/email.ts
+
+export const emailTemplate = {
+  structure: [
+    'Subject',
+    'Preview text',
+    'Recognition',
+    'Reframe',
+    'Teaching',
+    'Application',
+    'Invitation',
+  ],
+} as const
