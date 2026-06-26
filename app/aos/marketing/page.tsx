e9a47e59de6efb5bdc2f06208f6acf0bd93663da
anastasis-elite <lyndsbelle@gmail.com>
2026-06-26 13:18:17 -0500

    Update page.tsx

--- app/aos/marketing/page.tsx
+++ app/aos/marketing/page.tsx
@@ -1,11 +1,6 @@
 // app/aos/marketing/page.tsx
 
-import MarketingEngine from '@/components/marketing/marketing-engine'
 import VideoContentEngine from '@/components/marketing/VideoContentEngine'
-
-export default function MarketingPage() {
-  return <MarketingEngine />
-}
 
 export default function MarketingPage() {
   return <VideoContentEngine />
