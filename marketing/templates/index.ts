59a9194206b3ba99b84f840f232e248ea29edb7e
anastasis-elite <lyndsbelle@gmail.com>
2026-06-26 12:25:30 -0500

    Create index.ts

---
+++ marketing/templates/index.ts
@@ -0,0 +1,6 @@
+// marketing/templates/index.ts
+
+export { captionTemplate } from './caption'
+export { scriptTemplate } from './script'
+export { carouselTemplate } from './carousel'
+export { emailTemplate } from './email'
