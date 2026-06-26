6ea352f0895eea86ca8b4093834468e406b9e113
anastasis-elite <lyndsbelle@gmail.com>
2026-06-26 12:24:54 -0500

    Create carousel.ts

---
+++ marketing/templates/carousel.ts
@@ -0,0 +1,13 @@
+// marketing/templates/carousel.ts
+
+export const carouselTemplate = {
+  structure: [
+    'Belief interruption cover',
+    'Old belief',
+    'What is actually happening',
+    'Human Engineering principle',
+    'Examples',
+    'Reframe',
+    'Invitation',
+  ],
+} as const
