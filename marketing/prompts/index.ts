74031b5bd7090428fc3c429d781b9729e6d091d8
anastasis-elite <lyndsbelle@gmail.com>
2026-06-26 12:22:46 -0500

    Create index.ts

---
+++ marketing/prompts/index.ts
@@ -0,0 +1,11 @@
+// marketing/prompts/index.ts
+
+export { reasoningEnginePrompt } from './reasoning-engine'
+export { contentStrategistPrompt } from './content-strategist'
+export { hookGeneratorPrompt } from './hook-generator'
+export { captionGeneratorPrompt } from './caption-generator'
+export { scriptGeneratorPrompt } from './script-generator'
+export { platformAdapterPrompt } from './platform-adapter'
+export { contentAuditorPrompt } from './content-auditor'
+
+export type { PromptContext, PromptWorker } from './types'
