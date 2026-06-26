781e3e3415d4c4758e8957af95360f9a7a7b99e7
anastasis-elite <lyndsbelle@gmail.com>
2026-06-26 12:24:37 -0500

    Create script.ts

---
+++ marketing/templates/script.ts
@@ -0,0 +1,15 @@
+// marketing/templates/script.ts
+
+export const scriptTemplate = {
+  structure: [
+    'OST',
+    'Opening line',
+    'Recognition',
+    'Reframe',
+    'Example',
+    'CTA',
+  ],
+
+  rule:
+    'Every script should be clear enough to film without overthinking.',
+} as const
