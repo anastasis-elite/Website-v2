d58a2cba449356c4f01768bd04451f1de29dcdff
anastasis-elite <lyndsbelle@gmail.com>
2026-07-27 11:52:47 -0500

    Update IgniteDashboard.tsx

--- components/program-dashboard/IgniteDashboard.tsx
+++ components/program-dashboard/IgniteDashboard.tsx
@@ -148,7 +148,7 @@ function HydrationRing({
         aria-expanded={open}
         style={{
           appearance: 'none',
-          background: 'transparent',
+          background: '',
           border: 0,
           color: 'inherit',
           padding: 0,
